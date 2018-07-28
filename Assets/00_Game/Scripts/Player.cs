239590522e7c1642f97394459d8940c7e37fd4c7
Akirame <gsvillalba12@hotmail.com>
2018-07-28 14:45:34 -0300

    Initial commit

---
+++ Assets/00_Game/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region singleton
+    static private Player instance;
+    static public Player Get()
+    {
+        return instance;
+    }
+    private void Awake()
+    {
+        if (!instance)
+            instance = this;
+        else
+            Destroy(this);
+    }
+    #endregion
+
+    public float time;
+    public float speed;
+
+    private float timer;
+    private bool moving;
+    private Vector2 moveDir;
+
+    private void Start()
+    {
+        timer = 0;
+        moving = false;
+    }
+    void Update ()
+    {
+        MoveController();
+	}
+
+    private void MoveController()
+    {
+        if (!moving)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                moving = true;
+                moveDir = Vector2.up;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                moving = true;
+                moveDir = Vector2.down;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                moving = true;
+                moveDir = Vector2.left;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                moving = true;
+                moveDir = Vector2.right;
+            }
+        }
+        else
+        {
+            if (timer < time)
+            {
+                timer += Time.deltaTime;
+                Vector3 dir = moveDir;
+                transform.position += dir * speed * Time.deltaTime;
+            }
+            else
+            {
+                timer = 0;
+                moving = false;
+            }
+        }
+    }
+}
