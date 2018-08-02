d694da0ed6d930d0dd12c2892ea71af7540d6d02
Akirame <gsvillalba12@hotmail.com>
2018-08-02 17:34:03 -0300

    Add CameraFollow

--- Assets/00_Game/Scripts/CameraController.cs
+++ Assets/00_Game/Scripts/CameraController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     #region singleton
     private static CameraController instance;
@@ -19,6 +20,29 @@ public class CameraController : MonoBehaviour {
     }
     #endregion
 
+
+    public Transform firstWall;
+    public Transform secondWall;
+    private Transform playerToFollow;
+    private float min;
+    private float max;
+    public float num;
+
+    void Start()
+    {
+        playerToFollow = Player.Get().gameObject.transform;
+        min = firstWall.position.y + num;
+        max = secondWall.position.y - num;
+    }
+
+    void Update()
+    {
+        if (playerToFollow.transform.position.y > min && playerToFollow.transform.position.y < max)
+        {
+            Vector3 newPos = new Vector3(0, playerToFollow.transform.position.y, -10);
+            transform.position = newPos;
+        }
+    }
     public Camera GetViewport()
     {
         return GetComponent<Camera>();
